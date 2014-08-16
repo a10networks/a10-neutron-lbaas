2b8a4c0218940c93f7a55adb6a2aafc467553298
Doug Wiegley <dougw@a10networks.com>
2014-08-16 02:08:17 -0600

    bump client req

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "a10-neutron-lbaas",
-    version = "1.1.2",
+    version = "1.1.3",
     packages = find_packages(),
 
     author = "A10 Networks",
