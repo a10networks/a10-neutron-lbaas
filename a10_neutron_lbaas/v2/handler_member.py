a173e0cbcd3059d5b12a4d93e9f9b520087b66bd
Doug Wiegley <doug@parksidesoftware.com>
2015-05-01 16:38:42 -0600

    always build uphill objects

--- a10_neutron_lbaas/v2/handler_member.py
+++ a10_neutron_lbaas/v2/handler_member.py
@@ -59,7 +59,13 @@ class MemberHandler(handler_base_v2.HandlerBaseV2):
 
     def create(self, context, member):
         with a10.A10WriteStatusContext(self, context, member) as c:
-            self._create(c, context, member)
+            for i in range(0, 2):
+                try:
+                    self._create(c, context, member)
+                except acos_errors.NotFound:
+                    self.a10_driver.pool._create(c, context, member.pool)
+                    continue
+                break
 
     def update(self, context, old_member, member):
         with a10.A10WriteStatusContext(self, context, member) as c:
