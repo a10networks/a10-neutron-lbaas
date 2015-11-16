e9e157c129b7007e26661057a1b36056cdd1e328
Cedric Shock <cshock@a10networks.com>
2015-11-16 18:43:04 +0000

    Coverage: test uncallable mock used in migrations
    use base TestCase with assertIn for all the TestBases.

--- a10_neutron_lbaas/tests/test_case.py
+++ a10_neutron_lbaas/tests/test_case.py
@@ -1,4 +1,4 @@
-# Copyright 2015,  A10 Networks
+# Copyright (C) 2015, A10 Networks Inc. All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -10,19 +10,20 @@
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
-#    under the License.from neutron.db import model_base
+#    under the License.
 
-import a10_neutron_lbaas.tests.test_case as test_case
-
-import a10_neutron_lbaas.tests.db.session as session
-import sqlalchemy.orm
+import unittest
 
 
-class UnitTestBase(test_case.TestCase):
+def assertIn(expected, actual):
+    if expected not in actual:
+        raise Exception("Expected to find {0} in {1}".format(expected, actual))
 
-    def setUp(self):
-        self.connection = session.fake_migration_connection()
-        self.Session = sqlalchemy.orm.sessionmaker(bind=self.connection)
 
-    def tearDown(self):
-        self.connection.close()
+class TestCase(unittest.TestCase):
+    """unittest.TestCase with portable or custom assertions"""
+
+    def __init__(self, *args):
+        super(TestCase, self).__init__(*args)
+        if not hasattr(self, "assertIn"):
+            setattr(self, "assertIn", assertIn)
