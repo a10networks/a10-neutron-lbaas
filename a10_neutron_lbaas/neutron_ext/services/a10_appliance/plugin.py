0eda0cbcc144fb82fe2b6d7ff5e97bdd51896af4
Cedric Shock <cshock@a10networks.com>
2015-11-23 20:27:36 +0000

    A10 Appliance DB plugin mixin
    Tests for A10ApplianceDbMixin and plugin

--- a10_neutron_lbaas/neutron_ext/services/a10_appliance/plugin.py
+++ a10_neutron_lbaas/neutron_ext/services/a10_appliance/plugin.py
@@ -12,34 +12,44 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.from neutron.db import model_base
 
+from oslo_log import log as logging
+
 import a10_neutron_lbaas.neutron_ext.common.constants as constants
-import a10_neutron_lbaas.neutron_ext.extensions.a10_appliance as a10_appliance
+import a10_neutron_lbaas.neutron_ext.db.a10_appliance as a10_appliance
+
+LOG = logging.getLogger(__name__)
 
 
-class A10AppliancePlugin(a10_appliance.A10AppliancePluginBase):
+class A10AppliancePlugin(a10_appliance.A10ApplianceDbMixin):
 
     supported_extension_aliases = [constants.A10_APPLIANCE_EXT]
 
-    def __init__(self):
-        super(A10AppliancePlugin, self).__init__()
-
     def get_a10_appliances(self, context, filters=None, fields=None):
-        LOG.debug("A10AppliancePlugin.get_a10_appliances(): filters=%s, fields=%s", filters, fields)
-        raise NotSupportedException()
+        LOG.debug(
+            "A10AppliancePlugin.get_a10_appliances(): filters=%s, fields=%s",
+            filters,
+            fields)
+        return super(A10AppliancePlugin, self).get_a10_appliances(
+            context, filters=filters, fields=fields)
 
     def create_a10_appliance(self, context, a10_appliance):
-        LOG.debug("A10AppliancePlugin.create(): context=%s, id=%s", context, id)
-        raise NotSupportedException()
+        LOG.debug("A10AppliancePlugin.create(): a10_appliance=%s", a10_appliance)
+        return super(A10AppliancePlugin, self).create_a10_appliance(context, a10_appliance)
 
     def get_a10_appliance(self, context, id, fields=None):
-        LOG.debug("A10AppliancePlugin.get_a10_appliance(): context=%s, id=%s", context, id)
-        raise NotSupportedException()
+        LOG.debug("A10AppliancePlugin.get_a10_appliance(): id=%s, fields=%s", context, id, fields)
+        return super(A10AppliancePlugin, self).get_a10_appliance(context, id, fields=fields)
 
     def update_a10_appliance(self, context, a10_appliance_id, a10_appliance):
-        LOG.debug("A10AppliancePlugin.update_a10_appliance(): context=%s, cert=%s", context,
-                  a10_appliance)
-        raise NotSupportedException()
+        LOG.debug(
+            "A10AppliancePlugin.update_a10_appliance(): a10_appliance_id=%s, a10_appliance=%s",
+            a10_appliance_id,
+            a10_appliance)
+        return super(A10AppliancePlugin, self).update_a10_appliance(
+            context,
+            a10_appliance_id,
+            a10_appliance)
 
     def delete_a10_appliance(self, context, id):
-        LOG.debug("A10AppliancePlugin.delete(): context=%s, id=%s", context, id)
-        raise NotSupportedException()
+        LOG.debug("A10AppliancePlugin.delete(): id=%s", id)
+        return super(A10AppliancePlugin, self).delete_a10_appliance(context, id)
---
+++ a10_neutron_lbaas/tests/db/neutron_ext/db/test_a10_appliance.py
@@ -0,0 +1,179 @@
+# Copyright 2015,  A10 Networks
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.from neutron.db import model_base
+
+import mock
+
+import a10_neutron_lbaas.db.models as models
+import a10_neutron_lbaas.tests.db.test_base as test_base
+
+import a10_neutron_lbaas.neutron_ext.common.constants as constants
+import a10_neutron_lbaas.neutron_ext.db.a10_appliance as a10_appliance
+import a10_neutron_lbaas.neutron_ext.extensions.a10Appliance as a10Appliance
+
+
+class TestA10ApplianceDbMixin(test_base.UnitTestBase):
+
+    def setUp(self):
+        super(TestA10ApplianceDbMixin, self).setUp()
+        self.plugin = a10_appliance.A10ApplianceDbMixin()
+
+    def context(self):
+        session = self.open_session()
+        context = mock.MagicMock(session=session, tenant_id='fake-tenant-id')
+        return context
+
+    def fake_appliance(self):
+        return {
+            'name': 'fake-name',
+            'description': 'fake-description',
+            'host': 'fake-host',
+            'api_version': 'fake-version',
+            'username': 'fake-username',
+            'password': 'fake-password'
+        }
+
+    def envelope(self, body):
+        return {a10Appliance.A10_APPLIANCE_RESOURCE: body}
+
+    def test_create_a10_appliance(self):
+        appliance = self.fake_appliance()
+        context = self.context()
+        result = self.plugin.create_a10_appliance(context, self.envelope(appliance))
+        context.session.commit()
+        self.assertIsNot(result['id'], None)
+        expected = appliance.copy()
+        expected.update(
+            {
+                'id': result['id'],
+                'tenant_id': context.tenant_id
+            })
+        self.assertEqual(expected, result)
+
+    def test_get_a10_appliance(self):
+        appliance = self.fake_appliance()
+        create_context = self.context()
+        create_result = self.plugin.create_a10_appliance(create_context, self.envelope(appliance))
+        create_context.session.commit()
+
+        context = self.context()
+        result = self.plugin.get_a10_appliance(context, create_result['id'])
+
+        self.assertEqual(create_result, result)
+
+    def test_get_a10_appliance_not_found(self):
+        context = self.context()
+        self.assertRaises(
+            a10Appliance.A10ApplianceNotFoundError,
+            self.plugin.get_a10_appliance,
+            context,
+            'fake-appliance-id')
+
+    def test_get_a10_appliances(self):
+        appliance = self.fake_appliance()
+        create_context = self.context()
+        create_result = self.plugin.create_a10_appliance(create_context, self.envelope(appliance))
+        create_context.session.commit()
+
+        context = self.context()
+        result = self.plugin.get_a10_appliances(context)
+
+        self.assertEqual([create_result], result)
+
+    def test_delete_a10_appliance(self):
+        appliance = self.fake_appliance()
+        create_context = self.context()
+        create_result = self.plugin.create_a10_appliance(create_context, self.envelope(appliance))
+        create_context.session.commit()
+
+        context = self.context()
+        self.plugin.delete_a10_appliance(context, create_result['id'])
+        context.session.commit()
+
+        get_context = self.context()
+        self.assertRaises(
+            a10Appliance.A10ApplianceNotFoundError,
+            self.plugin.get_a10_appliance,
+            get_context,
+            create_result['id'])
+
+    def test_delete_a10_appliance_not_found(self):
+        context = self.context()
+        self.assertRaises(
+            a10Appliance.A10ApplianceNotFoundError,
+            self.plugin.delete_a10_appliance,
+            context,
+            'fake-appliance-id')
+
+    def test_delete_a10_appliance_in_use(self):
+        appliance = self.fake_appliance()
+        create_context = self.context()
+        create_result = self.plugin.create_a10_appliance(create_context, self.envelope(appliance))
+        create_context.session.add(
+            models.default(
+                models.A10SLB,
+                a10_appliance_id=create_result['id']
+            ))
+        create_context.session.commit()
+
+        context = self.context()
+        self.assertRaises(
+            a10Appliance.A10ApplianceInUseError,
+            self.plugin.delete_a10_appliance,
+            context,
+            create_result['id'])
+
+    def test_update_a10_appliance(self):
+        appliance = self.fake_appliance()
+        create_context = self.context()
+        create_result = self.plugin.create_a10_appliance(create_context, self.envelope(appliance))
+        create_context.session.commit()
+
+        change = {
+            'api_version': 'other-version'
+        }
+        expected = create_result.copy()
+        expected.update(change)
+
+        context = self.context()
+        result = self.plugin.update_a10_appliance(context,
+                                                  create_result['id'],
+                                                  self.envelope(change))
+        context.session.commit()
+
+        self.assertEqual(expected, result)
+
+        get_context = self.context()
+        get_result = self.plugin.get_a10_appliance(get_context, create_result['id'])
+
+        self.assertEqual(expected, get_result)
+
+    def test_update_a10_appliance_not_found(self):
+        change = {
+            'api_version': 'other-version'
+        }
+        context = self.context()
+        self.assertRaises(
+            a10Appliance.A10ApplianceNotFoundError,
+            self.plugin.update_a10_appliance,
+            context,
+            'fake-appliance-id', self.envelope(change))
+
+    def test_get_plugin_name(self):
+        self.assertIsNot(self.plugin.get_plugin_name(), None)
+
+    def test_get_plugin_description(self):
+        self.assertIsNot(self.plugin.get_plugin_description(), None)
+
+    def test_get_plugin_type(self):
+        self.assertEqual(self.plugin.get_plugin_type(), constants.A10_APPLIANCE)
