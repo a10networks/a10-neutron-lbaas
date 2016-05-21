49dcd2d657fc3b28885e45dea8b702b34816b331
Cedric Shock <cshock@example.com>
2016-05-21 00:46:03 +0000

    Created at and updated at on all models

--- a10_neutron_lbaas/db/models.py
+++ a10_neutron_lbaas/db/models.py
@@ -90,13 +90,14 @@ class A10Base(Base):
         db = db_session or inspect(self).session
         db.delete(self)
 
+    created_at = sa.Column(sa.DateTime, default=_get_date, nullable=False)
+    updated_at = sa.Column(sa.DateTime, default=_get_date, onupdate=_get_date, nullable=False)
+
 
 class A10BaseMixin(object):
 
     id = sa.Column(sa.String(36), primary_key=True, nullable=False, default=_uuid_str)
     tenant_id = sa.Column(sa.String(36), nullable=False)
-    created_at = sa.Column(sa.DateTime, default=_get_date, nullable=False)
-    updated_at = sa.Column(sa.DateTime, default=_get_date, onupdate=_get_date, nullable=False)
 
 
 class A10TenantBinding(A10BaseMixin, A10Base):
