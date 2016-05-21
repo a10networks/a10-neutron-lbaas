94656cd6324e512a7d006d2a6c01173dbf079923
Michael Durrant <mdurrant@a10networks.com>
2016-05-21 20:50:24 +0000

    Fixed typo

--- a10_neutron_lbaas/v2/handler_pool.py
+++ a10_neutron_lbaas/v2/handler_pool.py
@@ -65,8 +65,7 @@ class PoolHandler(handler_base_v2.HandlerBaseV2):
         with a10.A10DeleteContext(self, context, pool) as c:
             debug_fmt = "handler_pool.delete(): removing member {0} from pool {1}"
             for member in pool.members:
-                debug_fmt.format(member, member.pool_id)
-                LOG.debug()
+                LOG.debug(debug_fmt.format(member, member.pool_id))
                 self.a10_driver.member._delete(c, context, member)
 
             LOG.debug("handler_pool.delete(): Checking pool health monitor...")
