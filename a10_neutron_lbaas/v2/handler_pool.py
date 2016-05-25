f526a42d87d87aff0607930b82923b07c8d1236a
Hunter Thompson <thompson.grey.hunter@gmail.com>
2016-05-25 23:18:52 +0000

    Removed unimportant logs

--- a10_neutron_lbaas/v2/handler_pool.py
+++ a10_neutron_lbaas/v2/handler_pool.py
@@ -128,7 +128,6 @@ class PoolHandler(handler_base_v2.HandlerBaseV2):
                 if name is not None:
                     oper_stats = c.client.slb.service_group.oper(name)
                     oper_stats = oper_stats["service-group"]["oper"]["state"]
-                    LOG.info("TRACER OPER_POOL")
                     lb_db.update_status(context, model_type, pool.id, operating_status=str(oper_stats))
-        except Exception:
-            pass
+        except Exception as ex:
+            LOG.exception(ex)
