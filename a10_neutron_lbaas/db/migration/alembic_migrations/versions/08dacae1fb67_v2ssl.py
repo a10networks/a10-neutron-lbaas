5dc9ce576a8e1982a906a41b5c1d0b028658194c
Michael Durrant <mdurrant@a10networks.com>
2016-06-21 06:42:26 +0000

    Updated model/migrations.

--- a10_neutron_lbaas/db/migration/alembic_migrations/versions/08dacae1fb67_v2ssl.py
+++ a10_neutron_lbaas/db/migration/alembic_migrations/versions/08dacae1fb67_v2ssl.py
@@ -23,10 +23,23 @@ def upgrade():
         sa.Column('created_at', sa.DateTime, nullable=False),
         sa.Column('updated_at', sa.DateTime, nullable=False),
         sa.Column('tenant_id', sa.String(36), nullable=False),
-        sa.Column('certificate_id', sa.String(36), nullable=False),
+        sa.Column('certificate_id', sa.String(36), nullable=False, sa.ForeignKey('a10_certificates.id')),
         sa.Column('listener_id', sa.String(36), nullable=False)
     )
 
+    op.create_table(
+        'a10_certificates',
+        sa.Column('tenant_id', sa.String(255), nullable=True),
+        sa.Column('id', sa.String(36), nullable=False),
+        sa.Column('name', sa.String(255), nullable=False),
+        sa.Column('description', sa.Text(1024), nullable=True),
+        sa.Column('cert_data', sa.Text(8000), nullable=False),
+        sa.Column('key_data', sa.Text(8000), nullable=False),
+        sa.Column('intermediate_data', sa.Text(8000), nullable=True),
+        sa.Column('password', sa.String(1024), nullable=True),
+
+    )
 
 def downgrade():
+    op.drop_table('a10_certificates')
     op.drop_table('a10_certificatelistenerbindings')
