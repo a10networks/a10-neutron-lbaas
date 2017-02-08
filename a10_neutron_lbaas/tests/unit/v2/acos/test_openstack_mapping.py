4db48a59d434ed4b4c43697c62c4d4c3b2c35560
Michael Durrant <mdurrant@a10networks.com>
2017-02-08 02:38:06 +0000

    v2 support for src-ip-hash mapping

---
+++ a10_neutron_lbaas/tests/unit/v2/acos/test_openstack_mapping.py
@@ -0,0 +1,27 @@
+# Copyright 2014, Doug Wiegley (dougwig), A10 Networks
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from a10_neutron_lbaas.acos import openstack_mappings as target
+from a10_neutron_lbaas.tests.unit import test_base
+
+import mock
+
+
+class TestA10Openstack(test_base.UnitTestBase):
+
+    def test_source_ip_v2(self):
+        mock_client = mock.Mock()
+        expected = 'src-ip-hash'
+        actual = target.service_group_lb_method_v2(mock_client, 'SOURCE_IP')
+        self.assertEqual(expected, actual)
