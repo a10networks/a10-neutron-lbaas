693b4ae6a5823a9f013bdb46a1e1bae92a517e53
Michael Durrant <mdurrant@a10networks.com>
2017-11-01 19:43:13 +0000

    version 1.6.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "a10-neutron-lbaas",
-    version = "1.6.4",
+    version = "1.6.5",
     packages = find_packages(),
 
     author = "A10 Networks",
