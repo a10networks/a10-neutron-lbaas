191567bf5efbfefb027867a5e7e3443021e5f2cc
omkartelee01 <omkareshwar.telee@a10networks.com>
2018-12-18 19:56:49 +0530

    Added unit tests

--- a10_neutron_lbaas/v2/handler_member.py
+++ a10_neutron_lbaas/v2/handler_member.py
@@ -66,11 +66,11 @@ class MemberHandler(handler_base_v2.HandlerBaseV2):
                     server_args['conn_limit'] = conn_limit
 
             if conn_resume:
-                if conn_resume == 0 or conn_resume == 1:
-                    server_args['conn_resume'] = conn_resume
-                else:
+                if conn_resume < 0 or conn_resume > 1000000:
                     LOG.warning("The specified conn_resume value is invalid. \
                     The value should be either 0 or 1")
+                else:
+                    server_args['conn_resume'] = conn_resume
 
             server_args = {'server': server_args}
             c.client.slb.server.create(server_name, server_ip,
