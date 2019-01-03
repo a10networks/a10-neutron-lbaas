488a50297114ab3c92d22c8d2d23cac0a7320bea
Michael Durrant <mdurrant@a10networks.com>
2019-01-03 23:51:48 +0000

    Added port # param to member tests

--- a10_neutron_lbaas/tests/unit/v1/test_handler_member.py
+++ a10_neutron_lbaas/tests/unit/v1/test_handler_member.py
@@ -65,11 +65,13 @@ class TestMembers(test_base.UnitTestBase):
         if conn_limit < 1 or conn_limit > 8000000:
             self.a.last_client.slb.server.create.assert_called_with(
                 name, ip,
+                '80',
                 status=status,
                 axapi_args={'server': {}})
         else:
             self.a.last_client.slb.server.create.assert_called_with(
                 name, ip,
+                '80',
                 status=status,
                 axapi_args={'server': {'conn-limit': conn_limit}})
         pool_name = self.a.member._pool_name(None, m['pool_id'])
